db8d5dfbf6f9efba0d6f0360432783c7aa92f72a
Sherjil Ozair <sherjilozair@gmail.com>
2018-04-30 18:18:59 -0400

    Add SDL symbols, clang bindings

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Parse C Headers using Clang Python Bindings
 
+## Usage
 - `python parser.py headers/SDL.h`
 
 ## Roadmap
 
-- wrote code generator (for Crystal)
-- handle declarations other than function
-- Figure out what is considered public API
+- [ ] Handle symbols other than function.
+- [ ] Write code generator for Crystal.
+- [X] Figure out what is considered public API.
--- parser.py
+++ parser.py
@@ -3,50 +3,40 @@ import sys
 # include https://github.com/llvm-mirror/clang/tree/master/bindings/python in PYTHONPATH
 from clang.cindex import CursorKind, Index
 
-def process_structs(node):
-  return []
-
-
 def process_function(node):
   params = [node.type.spelling + " " + node.spelling for node in node.get_children() if node.kind == CursorKind.PARM_DECL]
-  return [node.type.spelling + " " + node.spelling + "(" + ", ".join(params) + ")"]
+  print(node.type.spelling + " " + node.spelling + "(" + ", ".join(params) + ")")
 
 
 def process_enum(node):
-  return []  # TODO
+  pass
 
 
-def process_typedef(node):
-  return [] # TODO
+def process_hint(node):
+  pass
 
 
-def process_union(node):
-  return []  # TODO
+def process_structs(node):
+  pass
 
-
-def walk_SDL(node):
-  if node.kind == CursorKind.STRUCT_DECL:
-    return process_structs(node)
-  if node.kind == CursorKind.FUNCTION_DECL:
-    return process_function(node)
-  if node.kind == CursorKind.ENUM_CONSTANT_DECL:
-    return process_enum(node)
-  if node.kind == CursorKind.TYPEDEF_DECL:
-    return process_typedef(node)
-  if node.kind == CursorKind.UNION_DECL:
-    return process_union(node)
-  else:
-    return []
 
 def walk(node):
-  if node.spelling.startswith("SDL_"):
-    return walk_SDL(node)
-  else:
-    return sum([walk(child) for child in node.get_children()], [])
-      
+  for i in range(4):
+    if node.spelling in syms[i]:
+      processors[i](node)
+  for n in node.get_children():
+    walk(n)
 
-index = Index.create()
-tu = index.parse(sys.argv[1])
-li = walk(tu.cursor)
-for n in li:
-  print(n)
+
+if __name__ == '__main__':
+
+  func_syms = set(open("symbols/functions.txt").read().split('\n'))
+  enum_syms = set(open("symbols/enums.txt").read().split('\n'))
+  hint_syms = set(open("symbols/hints.txt").read().split('\n'))
+  struct_syms = set(open("symbols/structs.txt").read().split('\n'))
+  syms = [func_syms, enum_syms, hint_syms, struct_syms]
+  processors = [process_function, process_enum, process_hint, process_structs]
+
+  index = Index.create()
+  tu = index.parse(sys.argv[1])
+  walk(tu.cursor)
